a4a66b8e8275dd03eeab8e0e6a27be43679a9fed
Joe <joe@00000111.co.uk>
2021-08-26 15:47:13 +0100

    Changed some error messages and structure
    Removed some joke commands
    Changed structure
    Added some comments
    Removed some channel specific stuff
    Fixed Spam message

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ import datetime;
 
 
 client = discord.Client()
-channel = client.get_channel(cn.staff)
 temp = []
 
 def main():
     global df
     df = pd.read_csv('worldcities.csv', index_col=0)
     df = df.drop(columns=["iso2","iso3","capital","population","id"])
-    global channel
     
 if __name__ == '__main__':
     main()
@@ -28,67 +26,64 @@ if __name__ == '__main__':
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-def validStr(city, country):
-    if(city != "" and city.isnumeric() == False and country != "" and country.isnumeric() == False):
-        return True
-    else:
-        return False
-
-def add(city, country):
-    res = df.query("(city_ascii=='%s' | admin_name=='%s') & country=='%s'" % (city, city, country))
-    return res
-
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    print(message.author)
-    
-    if message.channel == client.get_channel(cn.staff) and  message.content.startswith('!add'):
+    if message.content.startswith('!add'):
+
+        #Send Log Message
         embedVar = discord.Embed(title="New Command to MaxMap", color=0xff0080)
         embedVar.set_author(name=(message.author.nick + " | " + message.author.name + message.author.discriminator), icon_url=message.author.avatar_url)
         embedVar.add_field(name="Message:", value=message.content, inline=False)
         embedVar.add_field(name="Time:", value=str(datetime.datetime.now()), inline=False)
         await client.get_channel(cn.spam).send(embed=embedVar)
 
-    if message.content.startswith('!add'):
+        #Process String
         global sended
         sended = message
         strip = message.content.replace("!add", "")
         if strip == "":
             await sended.channel.send("Please enter a city and country - For help do !maphelp")
         else:
+            #Title text and split into list based on comma delim
             strip = strip.title()
             split = strip.split(',')
+            #Checks if only one is entered, needs both.
             if(len(split) == 1 or split[1] == ""):
                 await sended.channel.send("Please enter a city and country - For help do !maphelp")
             else:
-                city = split[0]
-                country = split[1]
-                city = city.lstrip(" ")
-                country = country.lstrip(" ")
+                #Removes any extra spacing
+                city = split[0].lstrip(" ")
+                country = split[1].lstrip(" ")
 
+                #Further checks.
                 if(validStr(city, country)):
+                    #Fetch City from spreadsheet, if empty error, if more than one choose first.
                     res = add(city, country)
                     if(res.empty):
-                        await sended.channel.send((city + ", " + country + ' not found, check spelling - Or try English spelling.'))
+                        await sended.channel.send((city + ", " + country + ' not found, check spelling - Try without symbols, ASCII characters only.'))
                     else:
                         if(res.shape[1] > 1):
                             res = res.iloc[0]
                         try: 
+                            #Check dataset with current city, pass through temp array too
                             mb.checkDataset(res, temp)
                             try:
+                                #Append the return value to temp
                                 ret = mb.addToDataset(res)
                                 temp.append(ret)
-                                await sended.channel.send("City Added to Map! - Give it a few minutes to update.")
+                                #Send update to channel sent from
+                                await sended.channel.send("City/Town Added to Map! - Give it a few minutes to update.")
                             except Exception as e:
                                 await sended.channel.send(e)
                         except Exception as e:
                             await sended.channel.send(e)
 
+    #Extra Commands
     if message.content == '!maphelp':
-        helpembed = discord.Embed(title="MaxMap Help", description="**Type in '!add', followed by your city (above 10,000 population), and the country it is in.** \n **For example:** !add Nottingham, United Kingdom. \n\n Please note, the dataset that I am using for Cities and Countries is public, and so it may not be up-to-date or politically correct.",color=0xff0080)
+        helpembed = discord.Embed(title="MaxMap Help", description="**Type in '!add', followed by your city/town (above 10,000 population), and the country it is in.** \n **For example:** !add Nottingham, United Kingdom. \n\n Please note, the dataset that I am using for Cities and Countries is public, and so it may not be up-to-date or politically correct.",color=0xff0080)
         helpembed.set_footer(text="If you are having issues message/mention @jozef")
         await message.channel.send(embed=helpembed)
         
@@ -97,21 +92,20 @@ async def on_message(message):
         embedVar.set_thumbnail(url="http://www.cs.nott.ac.uk/~pszmw/MaxMap/Web/map.html")
         await message.channel.send(embed=embedVar)
 
-    if message.content == '!reecedowhatyouwannado' and message.channel == client.get_channel(cn.mentor):
-        await message.channel.send("Reece do a phd haha xD <@281693374116003841>")
-
     if message.content == '!reece' and message.channel == client.get_channel(cn.mentor):
         await message.channel.send("https://cdn.discordapp.com/attachments/859436872597897257/880154423958061127/reece.png")
     
-    if message.content == '!max' and message.channel == client.get_channel(cn.mentor):
-        await message.channel.send("https://cdn.discordapp.com/attachments/859436872597897257/880154794097000528/MaxMapIcon.png")
+#Checks string size and value for both city and country
+def validStr(city, country):
+    if(city != "" and city.isnumeric() == False and country != "" and country.isnumeric() == False):
+        return True
+    else:
+        return False
 
-    if message.content == '!comm' and message.author.id == 155339919735128065:
-        strip = message.content.replace("!comm", "")
-        split = strip.split(',')
-        channel = split[0].lstrip(" ")
-        msg = split[1].lstrip(" ")
-        channel = client.get_channel(channel)
-        channel.send(msg)
+#Check DF based on a few columns and return result
+def add(city, country):
+    res = df.query("(city_ascii=='%s' | admin_name=='%s') & country=='%s'" % (city, city, country))
+    return res
 
+   
 client.run(cn.DISCORD_BOT_TOKEN)
