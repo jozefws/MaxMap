267c7b83bfc33bd374994aa58d44b53fa519bb16
jozefws <joe@00000111.co.uk>
2022-08-23 22:38:21 +0000

    Added a James Harvey special message.

--- constants.js
+++ constants.js
@@ -1,2 +1,4 @@
+
+// This means public key not private btw lol. Mapbox uses SK for private(secret) keys.
 var MAPBOX_TOKEN = "pk.eyJ1Ijoiam96ZWYtNyIsImEiOiJja3NueXU0YngwaTVxMnhvNmV5NHdpMjMxIn0.9f-rE6J2kHbeONG25YJZfA"
 var MAPBOX_DATASETID = "cl47pnzki00xs28nz3zpk6q23"
